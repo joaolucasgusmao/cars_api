7d383b137d7eee22eb06f338b8ba0060c212dd29
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-03-10 02:13:01 -0300

    fix: cors correctly applied at api

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
+// import "reflect-metadata";
 import "express-async-errors";
 import express, { json } from "express";
 import helmet from "helmet";
-// import cors from "cors";
+import cors from "cors";
 import { carRouter } from "./routes";
 import { handleErrors } from "./middlewares";
 
 export const app = express();
 
-// app.use(cors);
-app.use(json());
+app.use(cors());
 app.use(helmet());
+app.use(json());
 
 app.use("/cars", carRouter);
 
